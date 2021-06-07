898eb687e3cffa91b2be991390271dfffd83c03b
Kevin <mac@MacBook-Pro-cua-Mac.local>
2021-06-07 11:02:43 +0700

    update

--- src/helpers.php
+++ src/helpers.php
@@ -1,7 +1,10 @@
 <?php
-function ddcors(...$args) {
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: *');
-    header('Access-Control-Allow-Headers: *');
-    dd(...$args);
+if(!function_exists("ddcors")){
+    function ddcors(...$args) {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: *');
+        header('Access-Control-Allow-Headers: *');
+        dd(...$args);
+    }
 }
+
